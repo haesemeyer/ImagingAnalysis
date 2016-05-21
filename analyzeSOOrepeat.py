750988a8fef62ca8b05d1dbd7b7d9f2180141e8b
Martin Haesemeyer <m.haesemeyer@gmail.com>
2016-05-21 16:32:59 -0400

    Added plots for swim triggered averages and behavior. Compute stimulus correlations across whole experiment instead of versus averaged trace.

--- analyzeSOOrepeat.py
+++ analyzeSOOrepeat.py
@@ -21,6 +21,9 @@ sys.path.append('C:/Users/mhaesemeyer/Documents/Python Scripts/BehaviorAnalysis'
 
 import mhba_basic as mb
 
+from mh_lasTemp import LaserTempData
+
+
 
 def LoadGraphTailData(graph):
     name = graph.SourceFile[:-6]+".tail"
@@ -81,16 +84,55 @@ def ComputeAveragedTimeseries(timeseries):
     return np.sum(np.reshape(timeseries[0:l-n_hangoverFrames],(2,(l-1)//2)),0)#sum/mean equivalent here (anyways determined by graph size)
 
 
-def ComputeStimInducedNoise(graph,avg_timeseries):
+#def ComputeStimInducedNoise(graph,avg_timeseries):
+#    """
+#    Using information about pre, stim and post frames
+#    from the graph computes the ratio of standard
+#    deviations of timeseries between pre frames
+#    and stimulus-presentation frames (stim+post)
+#    """
+#    s_pre = np.std(avg_timeseries[:graph.FramesPre])
+#    s_stim = np.std(avg_timeseries[graph.FramesPre:])
+#    return s_stim / s_pre
+
+def ComputeStimulusEffect(graph,timeseries):
     """
-    Using information about pre, stim and post frames
-    from the graph computes the ratio of standard
-    deviations of timeseries between pre frames
-    and stimulus-presentation frames (stim+post)
+    Tries to address whether the activity of a unit
+    gets mostly influenced by the stimulus. To that end
+    compares the variation in the two pre-stimulus periods
+    with the variation between stimulus periods and their
+    given pre periods
     """
-    s_pre = np.std(avg_timeseries[:graph.FramesPre])
-    s_stim = np.std(avg_timeseries[graph.FramesPre:])
-    return s_stim / s_pre
+    l = timeseries.size
+    reps = np.reshape(timeseries[0:l-n_hangoverFrames],(2,(l-1)//2))
+    m_pre = np.mean(reps[:,:graph.FramesPre],1)
+    m_stim = np.mean(reps[:,graph.FramesPre:],1)
+    d_pre = np.abs(m_pre[0]-m_pre[1])
+    d_ps = np.mean(np.abs(m_pre-m_stim))
+    return d_ps / d_pre
+
+def ComputeSwimTriggeredAverage(graph_list,tailData,f_pre,f_post):
+    mta = []
+    for g in graph_list:
+        td = tailData[g.SourceFile]
+        if td.boutFrames is None or td.boutFrames.size == 0:
+            continue
+        im_len = g.RawTimeseries.size
+        gts = (g.RawTimeseries-np.percentile(g.RawTimeseries,20))/np.percentile(g.RawTimeseries,20)
+        if np.any(np.isnan(gts)) or np.any(np.isinf(gts)):
+            continue
+        ix = mb.IndexingMatrix(td.boutFrames,f_pre,f_post,im_len)[0]
+        btrig = gts[ix]
+        mta.append(np.nanmean(btrig,0))
+    return np.vstack(mta)
+
+from scipy.signal import lfilter
+
+def CorrelateResWithMTA(graph,mta):
+    rs = graph.RawTimeseries
+    rs = (rs-np.mean(rs))/np.std(rs)
+    mta = (mta-np.mean(mta))/np.std(mta)
+    return lfilter(mta[::-1],1,rs)
 
 def ComputeDFF(avg_timeseries):
     f0 = np.mean(avg_timeseries[:72])
@@ -222,7 +264,7 @@ if __name__ == "__main__":
     #1) In focused regions such as the trigeminal, large activity spikes cause poor scores even in the absence of movement (manual check)
     #2) If the eyes are contained in the stack, as they can move within the agarose they can corrupt the score
     #=> Until better metric is found set to large value...
-    qual_cut = 1#maximum quality score deviation to still consider graph valid
+    qual_cut = 0.1#maximum quality score deviation to still consider graph valid
     n_shuffles = 200
 
     #load unit activity from each imaging plane, compute motor correlations
@@ -231,11 +273,18 @@ if __name__ == "__main__":
     graphFiles = UiGetFile([('PixelGraphs','.graph')],multiple=True)
     graph_list = []
 
+    #create vector describing stimulus phase of experiment. TODO: Define same way as stimOn below, dependent on graph
+    phase = np.zeros((72+144+180)*2+1)
+    phase[72:72+144] = 1
+    phase[72+144+36:72+144+72] = 1
+    phase[288+36:288+72] = 1
+    phase[72+144+180:-1] = phase[:72+144+180]
+
     for gf in graphFiles:
         try:
             f = open(gf,'rb')
             graphs = pickle.load(f)
-            if len(graphs)>0 and graphs[0].MaxQualScoreDeviation < qual_cut:#don't even process or add graphs from a plane that has a bad quality score
+            if len(graphs)>0:# and graphs[0].MaxQualScoreDeviation < qual_cut:#don't even process or add graphs from a plane that has a bad quality score
                 for g in graphs:
                     g.MotorCorrelation = np.corrcoef(g.RawTimeseries,g.PerFrameVigor)[0,1]
                     g.AveragedTimeseries = ComputeAveragedTimeseries(g.RawTimeseries)
@@ -251,18 +300,29 @@ if __name__ == "__main__":
                         stimOn[post_start+step_len:post_start+2*step_len] = 1
                         stimOn[post_start+3*step_len:post_start+4*step_len] = 1
                         stimOff = 1-stimOn
+                        stimOn = LaserTempData.PredictTemperature(3.625,0.53,stimOn,1/2.4)
+                        stimOff = LaserTempData.PredictTemperature(3.625,0.53,stimOff,1/2.4)
                         stimOn = CaConvolve(stimOn,g.CaTimeConstant,g.FrameRate)
+                        stimOn = stimOn / stimOn.max()
                         stimOff = CaConvolve(stimOff,g.CaTimeConstant,g.FrameRate)
+                        stimOff = stimOff / stimOff.max()
                         g.StimOn = stimOn
                         g.StimOff = stimOff
                     #compute correlation of responses and stimulus regressors
-                    step_start = g.FramesPre+g.FramesStim
-                    g.CorrOn = np.corrcoef(g.StimOn[step_start:],g.AveragedTimeseries[step_start:])[0,1]
-                    g.CorrOff = np.corrcoef(g.StimOff[step_start:],g.AveragedTimeseries[step_start:])[0,1]
+                    step_start1 = g.FramesPre + g.FramesStim
+                    step_end1 = step_start1 + g.FramesPost
+                    step_start2 = step_end1 + step_start1
+                    step_end2 = step_start2 + g.FramesPost
+                    con1 = np.corrcoef(g.StimOn[step_start1:],g.RawTimeseries[step_start1:step_end1])[0,1]
+                    con2 = np.corrcoef(g.StimOn[step_start1:],g.RawTimeseries[step_start2:step_end2])[0,1]
+                    g.CorrOn = (con1 + con2) / 2
+                    cof1 = np.corrcoef(g.StimOff[step_start1:],g.RawTimeseries[step_start1:step_end1])[0,1]
+                    cof2 = np.corrcoef(g.StimOff[step_start1:],g.RawTimeseries[step_start2:step_end2])[0,1]
+                    g.CorrOff = (cof1 + cof2) / 2
                     #compute fourier transform on the averaged timeseries
                     ComputeFourierAvgStim(g,g.FramesPre+g.FramesFFTGap,g.FramesPre+g.FramesStim,g.StimFrequency,g.FrameRate,True)
                     #compute stimulus induced increases in calcium fluctuations
-                    g.StimIndFluct = ComputeStimInducedNoise(g,g.AveragedTimeseries)
+                    g.StimIndFluct = ComputeStimulusEffect(g,g.RawTimeseries)
                     #create shuffles
                     g.ComputeGraphShuffles(n_shuffles)
                     sh_mc = np.zeros(n_shuffles)#motor correlations
@@ -272,10 +332,14 @@ if __name__ == "__main__":
                     sh_mfrac = np.zeros_like(sh_mc)#magnitude fraction
                     for i,row in enumerate(g.shuff_ts):
                         sh_mc[i] = np.corrcoef(row,g.PerFrameVigor)[0,1]
+                        con1 = np.corrcoef(g.StimOn[step_start1:],row[step_start1:step_end1])[0,1]
+                        con2 = np.corrcoef(g.StimOn[step_start1:],row[step_start2:step_end2])[0,1]
+                        sh_con[i] = (con1 + con2) / 2
+                        cof1 = np.corrcoef(g.StimOff[step_start1:],row[step_start1:step_end1])[0,1]
+                        cof2 = np.corrcoef(g.StimOff[step_start1:],row[step_start2:step_end2])[0,1]
+                        sh_coff[i] = (cof1 + cof2) / 2
+                        sh_StInFl[i] = ComputeStimulusEffect(g,row)
                         avg = ComputeAveragedTimeseries(row)
-                        sh_StInFl[i] = ComputeStimInducedNoise(g,avg)
-                        sh_con[i] = np.corrcoef(avg[step_start:],g.StimOn[step_start:])[0,1]
-                        sh_coff[i] = np.corrcoef(avg[step_start:],g.StimOff[step_start:])[0,1]
                         sh_mfrac[i] = ComputeTraceFourierFraction(avg,g.FramesPre+g.FramesFFTGap,g.FramesPre+g.FramesStim,g.StimFrequency,g.FrameRate,True)
                     g.sh_m_MotorCorrelation = np.mean(sh_mc)
                     g.sh_std_MotorCorrelation = np.std(sh_mc)
@@ -283,8 +347,8 @@ if __name__ == "__main__":
                     g.sh_std_CorrOn = np.std(sh_con)
                     g.sh_m_CorrOff = np.mean(sh_coff)
                     g.sh_std_CorrOff = np.std(sh_coff)
-                    g.sh_m_StIndFluct = np.mean(sh_StInFl)
-                    g.sh_std_StIndFluct = np.std(sh_StInFl)
+                    g.sh_m_StIndFluct = np.nanmean(sh_StInFl)
+                    g.sh_std_StIndFluct = np.nanstd(sh_StInFl)
                     g.sh_m_mfrac = np.mean(sh_mfrac)
                     g.sh_std_mfrac = np.std(sh_mfrac)
                     
@@ -307,8 +371,21 @@ if __name__ == "__main__":
     pot_stim_units = [g for g in non_mot_units if (g.StimIndFluct > g.sh_m_StIndFluct+2*g.sh_std_StIndFluct) and (g.mfrac_atStim > g.sh_m_mfrac + 2*g.sh_std_mfrac)]
 
     #identify on and off graphs based on respective correlation > 0.5
-    graph_on = [g for g in pot_stim_units if g.CorrOn>g.sh_m_CorrOn+2*g.sh_std_CorrOn]
-    graph_off = [g for g in pot_stim_units if g.CorrOff>g.sh_m_CorrOff+2*g.sh_std_CorrOff]
+    graph_on = [g for g in pot_stim_units if g.CorrOn>g.sh_m_CorrOn+2*g.sh_std_CorrOn and g.CorrOn>0.4]
+    graph_off = [g for g in pot_stim_units if g.CorrOff>g.sh_m_CorrOff+2*g.sh_std_CorrOff and g.CorrOff>0.4]
+
+    #create plot of average per-frame-swim vigor across experiments - count each plane only once
+    skip_dic = dict()
+    all_vigors = []
+    for g in graph_list:
+        if g.SourceFile in skip_dic:
+            continue
+        else:
+            l = g.PerFrameVigor.size
+            avg_vig = ComputeAveragedTimeseries(g.PerFrameVigor)
+            all_vigors.append(avg_vig)
+            skip_dic[g.SourceFile] = 1
+    all_vigors = np.vstack(all_vigors)
 
     ##manually remove graphs on eyes, etc.
     #from matplotlib.pyplot import pause
@@ -387,7 +464,7 @@ if __name__ == "__main__":
         pl.plot(np.mean(all_on,0),label='ON')
         pl.plot(np.mean(all_off,0),label='OFF')
         pl.xlabel('Frames - 48=20s')
-        pl.ylabel('Avg. ZScored response')
+        pl.ylabel('Avg. deltaF/F')
         pl.legend()
         pl.title('Average response of ON (heat-sens.) and OFF (cold-sens.) units')
 
@@ -403,3 +480,80 @@ if __name__ == "__main__":
         pl.xlabel('Frames - 48=20s')
         pl.ylabel('Stimulus laser [mA]')
         pl.title('Paradigm for each imaging plane')
+
+    #plot average swim vigor across planes
+
+    #load taildata
+    taildata = dict()
+    for g in graph_list:
+        if g.SourceFile in taildata:
+            continue
+        else:
+            taildata[g.SourceFile] = LoadGraphTailData(g)
+
+    #sort taildata: NOTE: Currently sorts all Z-Planes from different experiments together
+    def ZPIndex(fname):
+        try:
+            ix = int(fname[-8:-6])
+        except ValueError:
+            ix = int(fname[-7:-6])
+        return ix
+
+    std = [taildata[k] for k in sorted(taildata.keys(), key=ZPIndex)]
+
+    #create tail-vigor matrix for each plane - note: These won't align perfectly, so trim...
+    vig_lens = [t.vigor[t.scanFrame!=-1].size for t in std]
+    min_len = min(vig_lens)
+    vigor_mat = np.zeros((len(std),min_len))
+    for i,s in enumerate(std):
+        vigor_mat[i,:] = s.vigor[:min_len]
+
+    with sns.axes_style('white'):
+        fig, ax = pl.subplots()
+        m_vig = gaussian_filter1d(np.mean(mb.Bin1D(vigor_mat.copy(),2,axis=1),0),10)
+        time = np.linspace(0,m_vig.size/50,m_vig.size)
+        ax.plot(time,m_vig)
+        ax.plot(np.arange(phase.size)/2.4,phase*3)
+        ax.set_xlim(0,time.max())
+        ax.set_ylabel('Average swim vigor')
+        ax.set_xlabel('Time [s]')
+        sns.despine()
+
+    #create boxplot of stim and rest bout-frequencies
+    f_rest = np.array([restFreq(t) for t in std])
+    f_stim = np.array([stimFreq(t) for t in std])
+    with sns.axes_style('whitegrid'):
+        fig, ax = pl.subplots()
+        sns.boxplot(data=(f_rest,f_stim))
+
+    #movement triggered average
+    mta = ComputeSwimTriggeredAverage(motor_units,taildata,5,24)
+    mta_time = np.arange(-5,25)/2.4
+    with sns.axes_style('whitegrid'):
+        sns.tsplot(data=mta,time=mta_time)
+        pl.xlabel('Time around bout [s]');
+        pl.ylabel('deltaF/F')
+
+
+
+
+
+
+
+    ##check all
+    #from matplotlib.pyplot import pause
+    #skip_dic = dict()
+    #for i,g in enumerate(graph_list):
+    #    if g.SourceFile in skip_dic:
+    #        continue
+    #    PlotGraphInfo(g,i)
+    #    pause(0.5)
+    #    inp = ""
+    #    while inp!="s" and inp!="p" and inp!="c":
+    #        inp = input("[c]ontinue/[s]kip plane/sto[p]:")
+    #    if inp=="p":
+    #        break
+    #    elif inp=="s":
+    #        skip_dic[g.SourceFile] = 1
+    #    pl.close('all')
+    #    print(i/len(graph_list),flush=True)
