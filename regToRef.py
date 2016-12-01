d186d7cb9d8552dee2be956f929697324dd91a8c
Martin Haesemeyer <m.haesemeyer@gmail.com>
2016-12-01 13:28:29 -0500

    Increased registration stiffness (?) and made it dependent upon whether a Mid-HB or a FB stack is being registered

--- regToRef.py
+++ regToRef.py
@@ -45,6 +45,15 @@ def crop_heuristics(filename):
         return ''
 
 
+def regularization_heuristics(filename):
+    # tries to extract the appropriate engergy and jacobian weight based
+    # on the filename
+    if "FB" in filename:
+        return '--jacobian-weight 32e-5 --energy-weight 1'
+    else:
+        return '--jacobian-weight 8e-5 --energy-weight 5e-1'
+
+
 def assemble_commands(com_list):
     return ' '.join(com_list)
 
@@ -53,8 +62,8 @@ if __name__ == "__main__":
     ref_brain_path = "E:/Dropbox/ReferenceBrainCreation/H2BGc6s_Reference_8.nrrd"
     registration_commands_base = ['registration', '--dofs 6,9', '--verbose', '--exploration 30', '--accuracy 0.8',
                                   '--auto-multi-levels 4', '--initxlate', '--threads 6']
-    warp_commands_base = ['warp', '--grid-spacing 80', '--refine 3', '--jacobian-weight 5e-5', '--exploration 26',
-                          '--energy-weight 1e-1', '--coarsest 8', '--match-histograms', '--accuracy 0.8', '--verbose',
+    warp_commands_base = ['warp', '--grid-spacing 80', '--refine 3', '--exploration 26',
+                          '--coarsest 8', '--match-histograms', '--accuracy 0.8', '--verbose',
                           '--threads 6']
     reformat_commands_base = ['reformatx']
     float_names = UiGetFile([('Nrrd stack', '.nrrd')], multiple=True)
@@ -63,12 +72,13 @@ if __name__ == "__main__":
         wn = warp_name(fn)
         on = out_name(fn)
         crop_command = crop_heuristics(fn)
+        regul_command = regularization_heuristics(fn)
         reg_command = assemble_commands(registration_commands_base + [crop_command, make_output_command(an),
                                                                       ref_brain_path, fn])
         print(reg_command, flush=True)
         print("", flush=True)
         sp.run(reg_command)
-        warp_command = assemble_commands(warp_commands_base + [crop_command, make_output_command(wn),
+        warp_command = assemble_commands(warp_commands_base + [regul_command, crop_command, make_output_command(wn),
                                                                make_initial_command(an), ref_brain_path, fn])
         print("", flush=True)
         print(warp_command, flush=True)
