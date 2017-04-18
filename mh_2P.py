d8318a932ce0afe2ad0dc9cbc0d4db4edac1efaf
Martin Haesemeyer <m.haesemeyer@gmail.com>
2017-04-18 09:30:42 -0400

    Moved RegionContainer declaration

--- mh_2P.py
+++ mh_2P.py
@@ -4,8 +4,9 @@ from PIL import Image
 import pickle
 
 from collections import Counter
-
+import h5py
 import matplotlib.pyplot as pl
+import matplotlib.path as mpath
 import seaborn as sns
 
 from scipy.ndimage.filters import gaussian_filter
@@ -2176,6 +2177,70 @@ class KDTree:
         for i, p in enumerate(points):
             md[i] = np.mean(self.nearest_n_neighbors(p, n_neighbors, allow_0)[1])
         return md
+
+
+class RegionContainer:
+    """
+    Container for saving and loading RegionROI information
+    """
+
+    def __init__(self, positions, region_name: str, z_index: int):
+        """
+        Create a new RegionContainer
+        :param positions: The polygon vertices of the ROI
+        :param region_name: The name of this region
+        :param z_index: The index of the z-plane that this ROI came from
+        """
+        self.positions = positions
+        self.region_name = region_name
+        self.z_index = z_index
+
+    def point_in_region(self, point):
+        """
+        Tests whether a point is within the region or outside
+        :param point: The test point
+        :return: True if the point is within the region False if on a boundary or outside
+        """
+        # even when setting closed to True we still need to supply the first point twice
+        poly_path = mpath.Path(self.positions + [self.positions[0]], closed=True)
+        return poly_path.contains_point(point)
+
+    @staticmethod
+    def save_container_list(container_list, dfile: h5py.File):
+        """
+        Saves a list of RegionContainer objects to an hdf5 file
+        :param container_list: The list of RegionContainer objects to save
+        :param dfile: Handle of hdf5 file to which the list should be saved
+        """
+        key = 0
+        for rc in container_list:
+            while str(key) in dfile:
+                key += 1
+            dfile.create_group(str(key))
+            dfile[str(key)].create_dataset(name="positions", data=np.vstack(rc.positions))
+            dfile[str(key)].create_dataset(name="region_name", data=rc.region_name)
+            dfile[str(key)].create_dataset(name="z_index", data=rc.z_index)
+
+    @staticmethod
+    def load_container_list(dfile: h5py.File):
+        """
+        Loads a list of RegionContainer objects from an hdf5 file
+        :param dfile: Handle of hdf5 file from which list should be loaded
+        :return: A list of RegionContainer objects
+        """
+        container_list = []
+        for k in dfile.keys():
+            try:
+                pos = np.array(dfile[k]["positions"])
+                pos = [(p[0], p[1]) for p in pos]
+                rn = str(np.array(dfile[k]["region_name"]))
+                zi = int(np.array(dfile[k]["z_index"]))
+                rc = RegionContainer(pos, rn, zi)
+                container_list.append(rc)
+            except KeyError:
+                warnings.warn("Found non RegionContainer object in file {0}".format(dfile.filename))
+                continue
+        return container_list
 
 
 def UiGetFile(filetypes=[('Tiff stack', '.tif;.tiff')], multiple=False):
--- multiExpAnalysis.py
+++ multiExpAnalysis.py
@@ -15,9 +15,6 @@ import os
 import subprocess as sp
 from sklearn.cluster import SpectralClustering
 from sklearn.manifold import SpectralEmbedding
-import matplotlib.path as mpath
-import warnings
-import h5py
 
 sys.path.append('C:/Users/mhaesemeyer/Documents/Python Scripts/BehaviorAnalysis')
 from mhba_basic import Crosscorrelation
@@ -27,72 +24,6 @@ class max_cluster:
     def __init__(self, max_index):
         self.labels_ = max_index
         self.n_clusters = np.unique(self.labels_).size
-
-
-class RegionContainer:
-    """
-    Container for saving and loading RegionROI information
-    """
-
-    def __init__(self, positions, region_name: str, z_index: int):
-        """
-        Create a new RegionContainer
-        :param positions: The polygon vertices of the ROI
-        :param region_name: The name of this region
-        :param z_index: The index of the z-plane that this ROI came from
-        """
-        self.positions = positions
-        self.region_name = region_name
-        self.z_index = z_index
-
-    def point_in_region(self, point):
-        """
-        Tests whether a point is within the region or outside
-        :param point: The test point
-        :return: True if the point is within the region False if on a boundary or outside
-        """
-        # even when setting closed to True we still need to supply the first point twice
-        poly_path = mpath.Path(self.positions + [self.positions[0]], closed=True)
-        return poly_path.contains_point(point)
-
-    @staticmethod
-    def save_container_list(container_list, dfile: h5py.File):
-        """
-        Saves a list of RegionContainer objects to an hdf5 file
-        :param container_list: The list of RegionContainer objects to save
-        :param dfile: Handle of hdf5 file to which the list should be saved
-        """
-        key = 0
-        for rc in container_list:
-            while str(key) in dfile:
-                key += 1
-            dfile.create_group(str(key))
-            dfile[str(key)].create_dataset(name="positions", data=np.vstack(rc.positions))
-            dfile[str(key)].create_dataset(name="region_name", data=rc.region_name)
-            dfile[str(key)].create_dataset(name="z_index", data=rc.z_index)
-
-    @staticmethod
-    def load_container_list(dfile: h5py.File):
-        """
-        Loads a list of RegionContainer objects from an hdf5 file
-        :param dfile: Handle of hdf5 file from which list should be loaded
-        :return: A list of RegionContainer objects
-        """
-        container_list = []
-        for k in dfile.keys():
-            try:
-                pos = np.array(dfile[k]["positions"])
-                pos = [(p[0], p[1]) for p in pos]
-                rn = str(np.array(dfile[k]["region_name"]))
-                zi = int(np.array(dfile[k]["z_index"]))
-                rc = RegionContainer(pos, rn, zi)
-                container_list.append(rc)
-            except KeyError:
-                warnings.warn("Found non RegionContainer object in file {0}".format(dfile.filename))
-                continue
-        return container_list
-
-# Class RegionContainer
 
 
 def dff(fluomat, n_pre=5*60):
