a2e5a33730e36eae5ea74e438ae624a50e6f94a5
Martin Haesemeyer <m.haesemeyer@gmail.com>
2017-05-23 18:37:26 -0400

    Added various plots for the motor system and possibility to plot the color scale-bars for the fraction plots

--- anatomyPlots.py
+++ anatomyPlots.py
@@ -41,10 +41,16 @@ def build_all_region_labels():
     return np.array(labels)
 
 
-def PlotRegionHeatFraction_TopDown(rlabels, ax=None):
+def PlotRegionHeatFraction_TopDown(rlabels, ax=None, plot_color_bar=False):
+    pal = sns.color_palette("plasma", n_colors=15)
+    max_fraction = 0.15
+    if plot_color_bar:
+        sns.palplot(pal, 0.5)
+        cb_labels = np.linspace(0, max_fraction * 100, 15, endpoint=False)
+        pl.xticks(np.arange(15), [str(a) for a in cb_labels])
+        return None
     if ax is None:
         fig, ax = pl.subplots()
-    pal = sns.color_palette("plasma", n_colors=15)
     in_region = np.zeros(all_rl.shape[0], dtype=bool)
     if type(rlabels) is str:
         rlabels = [rlabels]
@@ -53,7 +59,7 @@ def PlotRegionHeatFraction_TopDown(rlabels, ax=None):
     total = in_region.sum()
     heat = np.logical_and(in_region, on_cells).sum() + np.logical_and(in_region, off_cells).sum()
     fraction = heat / total
-    col_index = int((fraction * 15) / 0.15)
+    col_index = int((fraction * 15) / max_fraction)
     if col_index > 14:
         col_index = 14
     # subsample
@@ -64,9 +70,39 @@ def PlotRegionHeatFraction_TopDown(rlabels, ax=None):
     return fraction, heat, total
 
 
+def PlotRegionMotorFraction_TopDown(rlabels, ax=None, plot_color_bar=False):
+    pal = sns.color_palette("plasma", n_colors=10)
+    max_fraction = 0.05
+    if plot_color_bar:
+        sns.palplot(pal, 0.5)
+        cb_labels = np.linspace(0, max_fraction * 100, 10, endpoint=False)
+        pl.xticks(np.arange(10), [str(a) for a in cb_labels])
+        return None
+    if ax is None:
+        fig, ax = pl.subplots()
+    in_region = np.zeros(all_rl.shape[0], dtype=bool)
+    if type(rlabels) is str:
+        rlabels = [rlabels]
+    for r in rlabels:
+        in_region = np.logical_or(in_region, all_rl == r)
+    total = in_region.sum()
+    motor = np.logical_and(in_region, membership_motor > -1).sum()
+    fraction = motor / total
+    col_index = int((fraction * 10) / max_fraction)
+    if col_index > 9:
+        col_index = 9
+    # subsample
+    ix_to_plot = np.arange(in_region.sum())
+    ix_to_plot = np.random.choice(ix_to_plot, ix_to_plot.size // 20)
+    ax.scatter(tf_centroids[in_region, 0][ix_to_plot], tf_centroids[in_region, 1][ix_to_plot], s=1, c=pal[col_index]
+               , alpha=0.3)
+    return fraction, motor, total
+
+
 def PlotRegionHeatFraction_Side(rlabels, ax=None):
     if ax is None:
         fig, ax = pl.subplots()
+    max_fraction = 0.15
     pal = sns.color_palette("plasma", n_colors=15)
     in_region = np.zeros(all_rl.shape[0], dtype=bool)
     if type(rlabels) is str:
@@ -76,7 +112,7 @@ def PlotRegionHeatFraction_Side(rlabels, ax=None):
     total = in_region.sum()
     heat = np.logical_and(in_region, on_cells).sum() + np.logical_and(in_region, off_cells).sum()
     fraction = heat / total
-    col_index = int((fraction * 15) / 0.15)
+    col_index = int((fraction * 15) / max_fraction)
     if col_index > 14:
         col_index = 14
     # subsample
@@ -85,6 +121,30 @@ def PlotRegionHeatFraction_Side(rlabels, ax=None):
     ax.scatter(tf_centroids[in_region, 1][ix_to_plot], tf_centroids[in_region, 2][ix_to_plot], s=1, c=pal[col_index],
                alpha=0.3)
     return fraction, heat, total
+
+
+def PlotRegionMotorFraction_Side(rlabels, ax=None):
+    if ax is None:
+        fig, ax = pl.subplots()
+    max_fraction = 0.05
+    pal = sns.color_palette("plasma", n_colors=10)
+    in_region = np.zeros(all_rl.shape[0], dtype=bool)
+    if type(rlabels) is str:
+        rlabels = [rlabels]
+    for r in rlabels:
+        in_region = np.logical_or(in_region, all_rl == r)
+    total = in_region.sum()
+    motor = np.logical_and(in_region, membership_motor > -1).sum()
+    fraction = motor / total
+    col_index = int((fraction * 10) / max_fraction)
+    if col_index > 9:
+        col_index = 9
+    # subsample
+    ix_to_plot = np.arange(in_region.sum())
+    ix_to_plot = np.random.choice(ix_to_plot, ix_to_plot.size // 20)
+    ax.scatter(tf_centroids[in_region, 1][ix_to_plot], tf_centroids[in_region, 2][ix_to_plot], s=1, c=pal[col_index],
+               alpha=0.3)
+    return fraction, motor, total
 
 
 if __name__ == "__main__":
@@ -265,4 +325,89 @@ if __name__ == "__main__":
     sns.barplot(data=pandas.DataFrame(off_fracs), ax=ax, order=labels)
     ax.set_ylim(0, 8)
     ax.set_ylabel("Fraction of OFF cells")
+    sns.despine(fig, ax)
+
+    # plot whole-brain overview of motor related cells
+    motor_store = h5py.File("H:/ClusterLocations_170327_clustByMaxCorr/motor_system.hdf5", "r")
+    membership_motor = np.array(motor_store["membership_motor"])[no_nan_aa]
+    ix_bground = np.arange(tf_centroids.shape[0])[stack_types == "MAIN"]
+    ix_bground = np.random.choice(ix_bground, ix_bground.size // 250, False)
+    fig, ax = pl.subplots()
+    ax.scatter(tf_centroids[ix_bground, 0], tf_centroids[ix_bground, 1], s=1, alpha=0.1, c='k')
+    ax.scatter(tf_centroids[np.logical_and(stack_types == "MAIN", membership_motor > -1), 0],
+               tf_centroids[np.logical_and(stack_types == "MAIN", membership_motor > -1), 1], s=1, alpha=0.4, c="C1")
+    ax.set_aspect('equal', 'datalim')
+    sns.despine(fig, ax)
+
+    fig, ax = pl.subplots()
+    half_brain = np.logical_and(tf_centroids[:, 0] < np.nanmedian(tf_centroids[stack_types == "MAIN", 0]),
+                                stack_types == "MAIN")
+    ax.scatter(tf_centroids[ix_bground, 1], tf_centroids[ix_bground, 2], s=1, alpha=0.1, c='k')
+    ax.scatter(tf_centroids[np.logical_and(half_brain, membership_motor > -1), 1],
+               tf_centroids[np.logical_and(half_brain, membership_motor > -1), 2], s=1, alpha=0.4, c="C1")
+    ax.set_aspect('equal', 'datalim')
+    sns.despine(fig, ax)
+
+    # Plot left-flick vs. right-flick cells
+    fig, ax = pl.subplots()
+    ax.scatter(tf_centroids[ix_bground, 0], tf_centroids[ix_bground, 1], s=1, alpha=0.1, c='k')
+    ax.scatter(tf_centroids[np.logical_and(stack_types == "MAIN", membership_motor == 2), 0],
+               tf_centroids[np.logical_and(stack_types == "MAIN", membership_motor == 2), 1], s=2, c="C2", label="Flick right")
+    ax.scatter(tf_centroids[np.logical_and(stack_types == "MAIN", membership_motor == 3), 0],
+               tf_centroids[np.logical_and(stack_types == "MAIN", membership_motor == 3), 1], s=2, c="C4", label="Flick left")
+    ax.set_aspect('equal', 'datalim')
+    ax.legend()
+    sns.despine(fig, ax)
+
+    # Plot stim_only vs. no_stim cells
+    fig, ax = pl.subplots()
+    ax.scatter(tf_centroids[ix_bground, 0], tf_centroids[ix_bground, 1], s=1, alpha=0.1, c='k')
+    ax.scatter(tf_centroids[np.logical_and(stack_types == "MAIN", membership_motor == 5), 0],
+               tf_centroids[np.logical_and(stack_types == "MAIN", membership_motor == 5), 1], s=2, c="C3", label="stim only")
+    ax.scatter(tf_centroids[np.logical_and(stack_types == "MAIN", membership_motor == 6), 0],
+               tf_centroids[np.logical_and(stack_types == "MAIN", membership_motor == 6), 1], s=2, c="C0", label="no stim")
+    ax.set_aspect('equal', 'datalim')
+    ax.legend()
+    sns.despine(fig, ax)
+
+    # plot map of motor-responsive fraction for different example regions - NOTE: ordered from ventral to dorsal!
+    r_to_plot = ["D_FB_L", "D_FB_R", "Hab_L", "Hab_R", "ncMLF", "Tect_L", "Tect_R", "A_HB_L", "A_HB_R",
+                 "Cerebellum_L", "Cerebellum_R"]
+    fig, ax = pl.subplots()
+    ix_bground = np.arange(tf_centroids.shape[0])[stack_types == "MAIN"]
+    ix_bground = np.random.choice(ix_bground, ix_bground.size // 100, False)
+    ax.scatter(tf_centroids[ix_bground, 0], tf_centroids[ix_bground, 1], s=1, alpha=0.2, c='k')
+    for r in r_to_plot:
+        PlotRegionMotorFraction_TopDown(r, ax)
+    ax.set_aspect('equal', 'datalim')
+    sns.despine(fig, ax)
+
+    fig, ax = pl.subplots()
+    r_side = ["D_FB_R", "Hab_R", "ncMLF", "Tect_R", "A_HB_R", "Cerebellum_R"]
+    ix_bground = np.arange(tf_centroids.shape[0])[stack_types == "MAIN"]
+    ix_bground = np.random.choice(ix_bground, ix_bground.size // 100, False)
+    ax.scatter(tf_centroids[ix_bground, 1], tf_centroids[ix_bground, 2], s=1, alpha=0.2, c='k')
+    for r in r_side:
+        PlotRegionMotorFraction_Side(r, ax)
+    ax.set_aspect('equal', 'datalim')
+    sns.despine(fig, ax)
+
+    r_to_count = [
+        ["A_HB_L", "A_HB_R"],
+        ["Cerebellum_L", "Cerebellum_R"],
+        "ncMLF",
+        ["Tect_L", "Tect_R"],
+        ["Hab_L", "Hab_R"],
+        ["D_FB_L", "D_FB_R"],
+        "SubPallium"
+    ]
+    labels = ["Ant. Hindbrain", "Cerebellum", "ncMLF", "Tectum", "Habenula", "Pallium", "Subpallium"]
+
+    mot_fracs = {labels[i]: [np.sum(np.logical_and(membership_motor > -1, part_of(r))) / np.sum(part_of(r)) * 100]
+                 for i, r in enumerate(r_to_count)}
+
+    fig, ax = pl.subplots()
+    sns.barplot(data=pandas.DataFrame(mot_fracs), ax=ax, order=labels)
+    ax.set_ylim(0, 6)
+    ax.set_ylabel("Fraction of motor related cells")
     sns.despine(fig, ax)
