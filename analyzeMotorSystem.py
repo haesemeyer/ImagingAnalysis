c3e8eb56970a108bf1fe7c744f8161758fbf54df
Martin Haesemeyer <m.haesemeyer@gmail.com>
2017-05-23 17:25:54 -0400

    Simplified code by immediately computing all cluster correlations not behavior first followed by stimulus dependence. Use different clustering only assigning cells to a more specialized cluster if they correlate *significantly* better to this cluster. Create motor cell regressors for later modeling and save to file.

--- analyzeMotorSystem.py
+++ analyzeMotorSystem.py
@@ -5,11 +5,99 @@ import seaborn as sns
 import h5py
 import pickle
 from mh_2P import MotorContainer, SLHRepeatExperiment
-from multiExpAnalysis import get_stack_types, dff, max_cluster, min_dist, create_centroid_stack
+from multiExpAnalysis import get_stack_types, min_dist
 from typing import List
 from motorPredicates import left_bias_bouts, right_bias_bouts, unbiased_bouts, high_bias_bouts
 import matplotlib as mpl
 from analyzeSensMotor import RegionResults
+
+
+class MotorCluster:
+    """
+    Class to cluster our motor-data by maximal correlation but before assigning to a sub-category testing whether
+    it is significantly better correlated to the sub-category than a broader category
+    Flick_Left(Right) -> Flicks -> All motor
+    Swims -> All motor
+    Heat_only -> All motor
+    NoHeat -> All motor
+    Assumes the following column structure of correlations:
+    All - Flicks - Right flicks - Left flicks - Swims - StimOnly - NoStim
+    """
+    def __init__(self, mc_corrs: np.ndarray, mc, all_activity, poa_on, poa_off, corr_thresh=0.6, p_cut=0.01):
+        self.cluster_labels = np.full(mc_corrs.shape[0], -1)
+        for i, c in enumerate(mc_corrs):
+            if np.nanmax(c) < corr_thresh:
+                continue
+            primary = np.nanargmax(c)
+            if primary == 0:
+                self.cluster_labels[i] = 0
+            else:
+                # need to perform significance tests
+                if primary == 1 or primary == 4:
+                    # potential flick or swim
+                    p = correlation_bootstrap_p(all_activity[i, :], mc[0][i], mc[primary][i])
+                    if p < p_cut:
+                        self.cluster_labels[i] = primary
+                    else:
+                        self.cluster_labels[i] = 0
+                elif primary == 5:
+                    # potential stim only
+                    p = correlation_bootstrap_p(all_activity[i, :], mc[0][i], mc[0][i] * poa_on)
+                    if p < p_cut:
+                        self.cluster_labels[i] = primary
+                    else:
+                        self.cluster_labels[i] = 0
+                elif primary == 6:
+                    # potential no stim
+                    p = correlation_bootstrap_p(all_activity[i, :], mc[0][i], mc[0][i] * poa_off)
+                    if p < p_cut:
+                        self.cluster_labels[i] = primary
+                    else:
+                        self.cluster_labels[i] = 0
+                elif primary == 2 or primary == 3:
+                    # potential flick right or left
+                    p = correlation_bootstrap_p(all_activity[i, :], mc[0][i], mc[primary][i])
+                    if p >= p_cut:
+                        self.cluster_labels[i] = 0
+                    else:
+                        p = correlation_bootstrap_p(all_activity[i, :], mc[1][i], mc[primary][i])
+                        if p < p_cut:
+                            self.cluster_labels[i] = primary
+                        else:
+                            self.cluster_labels[i] = 1
+                else:
+                    raise ValueError("Non recognized cluster number")
+
+
+def correlation_bootstrap_p(act, reg_1, reg_2, nboot=1000):
+    """
+    Computes the p-value that the correlation of act to reg_2 is larger than the correlation of act to reg_1
+    Args:
+        act: An activity trace
+        reg_1: The first regressor
+        reg_2: The second regressor
+        nboot: The number of bootstrap samples to generate
+
+    Returns:
+        p_value that c(act, reg2) > c(act, reg1)
+    """
+    if act.size != reg_1.size or reg_1.size != reg_2.size:
+        raise ValueError("All traces need to have the same length")
+    ix = np.arange(act.size)
+    all_d_corrs = np.zeros(act.size)
+    for i in range(nboot):
+        to_take = np.random.choice(ix, ix.size)
+        all_d_corrs[i] = np.corrcoef(act[to_take], reg_2[to_take])[0, 1]-np.corrcoef(act[to_take], reg_1[to_take])[0, 1]
+    return 1 - np.sum(all_d_corrs > 0) / nboot
+
+
+def zscore(mat, axis=1):
+    """
+    ZScore matrix mat along given axis
+    """
+    m = np.mean(mat, axis, keepdims=True)
+    s = np.std(mat, axis, keepdims=True)
+    return (mat-m)/s
 
 
 if __name__ == "__main__":
@@ -44,72 +132,48 @@ if __name__ == "__main__":
     mc_fl_left = MotorContainer(sourceFiles, itime, cat, predicate=right_bias_bouts, tdd=mc_all.tdd)
     mc_swims = MotorContainer(sourceFiles, itime, cat, predicate=unbiased_bouts, tdd=mc_all.tdd)
     mc = [mc_all, mc_flick, mc_fl_right, mc_fl_left, mc_swims]
-    labels = ["All motor", "Flicks", "Right flicks", "Left flicks", "Swims"]
+    labels = ["All motor", "Flicks", "Right flicks", "Left flicks", "Swims", "Stim only", "No stim"]
 
-    # perform stimulus-independent motor clustering and plot
-    motor_corrs = np.zeros((all_activity.shape[0], 5))
-    for i, act in enumerate(all_activity):
-        for j, m in enumerate(mc):
-            motor_corrs[i, j] = np.corrcoef(act, m[i])[0, 1]
-    # find cells that have at least one above-threshold motor correlation
-    motor_corrs[np.isnan(motor_corrs)] = 0
-    # motor_corrs[motor_corrs < 0.6] = 0
-    motor_cells = np.sum(motor_corrs > 0.6, 1) > 0
-    motor_corrs_sig = motor_corrs[motor_cells, :].copy()
-    # cluster by best regressor
-    km = max_cluster(np.nanargmax(motor_corrs_sig, 1))
-
-    # plot sorted by cluster identity
-    fig, ax = pl.subplots()
-    sns.heatmap(motor_corrs_sig[np.argsort(km.labels_), :], vmin=0, vmax=1, yticklabels=2500,
-                xticklabels=labels, cmap="viridis")
-    # plot cluster boundaries
-    covered = 0
-    for i in range(km.n_clusters):
-        covered += np.sum(km.labels_ == i)
-        ax.plot([0, motor_corrs_sig.shape[1] + 1], [km.labels_.size-covered, km.labels_.size-covered], 'k')
-    ax.set_title('Above threshold correlations clustered and sorted')
-
+    # perform motor clustering and plot - use motor categories as well as stimulus dependence
     # slow POA ON regressor for finding heat-specific motor units at different timescales
     # NOTE: Initially tested with trigeminal ON activity as well but POA seems to recover more cells
     storage = h5py.File('H:/ClusterLocations_170327_clustByMaxCorr/regiondata.hdf5', 'r')
-    poa_data = pickle.loads(np.array(storage["POA"]))
+    poa_data = pickle.loads(np.array(storage["POA"]))  # type: RegionResults
     poa_sens = np.mean(poa_data.region_acts[poa_data.region_mem == 1, :], 0)
     storage.close()
-    # we create the following two kinds of motor regressors:
-    # 1) A multiplication of mc_all with the activity trace (weighted by dFF)
-    # 2) A multiplication of mc_all with a 0/1 thresholded version of the activity trace (only events when active)
+    # we create the following stimulus-motor regressors:
+    # A multiplication of mc_all with a 0/1 thresholded version of the activity trace (only events when active)
     poa_thresh = 0.25  # POA slow ON cells are considered active above this threshold, based on looking at average
-    heat_motor_corrs = np.zeros((all_activity.shape[0], 2))
+    poa_on = poa_sens > 0.25
+    poa_off = poa_sens <= 0.25
+    motor_corrs = np.zeros((all_activity.shape[0], len(mc) + 2))
     for i, act in enumerate(all_activity):
-        # poa_act_mod_reg = mc_all[i] * poa_sens
-        poa_act_th_reg = mc_all[i] * (poa_sens > poa_thresh)
-        poa_nact_th_reg = mc_all[i] * (poa_sens < poa_thresh)
-        heat_motor_corrs[i, 0] = np.corrcoef(act, poa_act_th_reg)[0, 1]
-        heat_motor_corrs[i, 1] = np.corrcoef(act, poa_nact_th_reg)[0, 1]
-    heat_motor_corrs[np.isnan(heat_motor_corrs)] = 0
-    # heat_motor_corrs[heat_motor_corrs < 0.6] = 0
-    all_m_corrs = np.hstack((motor_corrs, heat_motor_corrs))
-    motor_cells = np.sum(all_m_corrs > 0.6, 1) > 0
-    motor_corrs_sig = all_m_corrs[motor_cells, :].copy()
-
-    km = max_cluster(np.nanargmax(motor_corrs_sig, 1))
+        for j, m in enumerate(mc):
+            motor_corrs[i, j] = np.corrcoef(act, m[i])[0, 1]
+        motor_corrs[i, len(mc)] = np.corrcoef(act, mc_all[i] * poa_on)[0, 1]
+        motor_corrs[i, len(mc) + 1] = np.corrcoef(act, mc_all[i] * poa_off)[0, 1]
+    # find cells that have at least one above-threshold motor correlation
+    motor_corrs[np.isnan(motor_corrs)] = 0
+    mclust = MotorCluster(motor_corrs, mc, all_activity, poa_on, poa_off)
+    motor_cells = (mclust.cluster_labels > -1)
+    l_in_cluster = mclust.cluster_labels[motor_cells]
+    motor_corrs_sig = motor_corrs[motor_cells, :].copy()
 
     # plot sorted by cluster identity
-    labels = ["All motor", "Flicks", "Right flicks", "Left flicks", "Swims", "Stim only", "No stim"]
     fig, ax = pl.subplots()
-    sns.heatmap(motor_corrs_sig[np.argsort(km.labels_), :], vmin=0, vmax=1, yticklabels=2500,
+    sns.heatmap(motor_corrs_sig[np.argsort(l_in_cluster), :], vmin=0, vmax=1, yticklabels=2500,
                 xticklabels=labels, cmap="viridis")
+    # plot cluster boundaries
     covered = 0
-    for i in range(km.n_clusters):
-        covered += np.sum(km.labels_ == i)
-        ax.plot([0, motor_corrs_sig.shape[1] + 1], [km.labels_.size-covered, km.labels_.size-covered], 'k')
+    for i in range(len(mc) + 2):
+        covered += np.sum(l_in_cluster == i)
+        ax.plot([0, motor_corrs_sig.shape[1] + 1], [l_in_cluster.size-covered, l_in_cluster.size-covered], 'k')
     ax.set_title('Above threshold correlations clustered and sorted')
 
     # create a membership array for plotting
     membership_motor = np.full_like(membership, -1)
     msh_nn = np.full_like(mship_nonan, -1)
-    msh_nn[motor_cells] = km.labels_
+    msh_nn[motor_cells] = l_in_cluster
     membership_motor[no_nan_aa] = msh_nn
 
     membership_heat_motor = membership_motor.copy()
@@ -148,3 +212,32 @@ if __name__ == "__main__":
     ax.set_aspect('equal', 'datalim')
     ax.legend()
     sns.despine(fig, ax)
+
+    tailstore.close()
+
+    # compute motor-type regressors
+    # NOTE: Since each plane and therefore each cell only covers a fraction of motor events we can't simply average
+    # dF/F values. This heavily biases the regressors to most active / brightest cells (SNR). Therefore sum z-scored
+    # versions of the traces
+    reg_all = np.mean(zscore(all_activity[mclust.cluster_labels == 0, :]), 0)
+    is_flick_cell = np.logical_and(mclust.cluster_labels > 0, mclust.cluster_labels < 4)
+    reg_flick = np.mean(zscore(all_activity[is_flick_cell, :]), 0)
+    reg_swim = np.mean(zscore(all_activity[mclust.cluster_labels == 4, :]), 0)
+    reg_stimOnly = np.mean(zscore(all_activity[mclust.cluster_labels == 5, :]), 0)
+    reg_noStim = np.mean(zscore(all_activity[mclust.cluster_labels == 6, :]), 0)
+    motor_type_regs = np.hstack(
+        (reg_all[:, None], reg_flick[:, None], reg_swim[:, None], reg_stimOnly[:, None], reg_noStim[:, None]))
+    # for later modeling, column normalize the regression matrix
+    motor_type_regs = zscore(motor_type_regs, 0)
+    swim_out = zscore(mc_swims.avg_motor_output, 0)
+    flick_out = zscore(mc_flick.avg_motor_output, 0)
+
+    # create a motor-system related file to store computed information
+    motor_store = h5py.File("H:/ClusterLocations_170327_clustByMaxCorr/motor_system.hdf5", "w-")
+    motor_store.create_dataset("motor_corrs", data=motor_corrs)
+    motor_store.create_dataset("cluster_labels", data=mclust.cluster_labels)
+    motor_store.create_dataset("membership_motor", data=membership_motor)
+    motor_store.create_dataset("motor_type_regs", data=motor_type_regs)
+    motor_store.create_dataset("swim_out", data=swim_out)
+    motor_store.create_dataset("flick_out", data=flick_out)
+    motor_store.close()
