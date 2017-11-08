4740b4d5604bc2a2d00755907c2f29d2352e5297
Martin Haesemeyer <m.haesemeyer@gmail.com>
2017-11-08 14:16:26 -0500

    Added the ability to co-align red channel stacks with their corresponding green channel stacks

--- realignAndProject.py
+++ realignAndProject.py
@@ -25,9 +25,23 @@ if __name__ == "__main__":
         print("Did not recognize input, defaulting to experimental stack", flush=True)
         is_ref = False
 
+    co_align = input("Should channel 1 stacks be co-aligned with channel 0 stacks? [y/n]")
+    if co_align == "y":
+        co_align = True
+    elif co_align == "n":
+        co_align = False
+    else:
+        print("Did not recognize input, default to separate alignment", flush=True)
+        co_align = False
+
+    if is_ref and co_align:
+        raise NotImplementedError("Co-alignment with reference stack generation currently not implemented")
+
     fnames = UiGetFile(multiple=True)
 
     for i, f in enumerate(fnames):
+        if co_align and "_1.tif" in f:
+            continue
         contFolder = path.dirname(f)  # the containing folder
         stackName = path.split(f)[1]
         save_dir = contFolder + "/projections"
@@ -35,12 +49,21 @@ if __name__ == "__main__":
             makedirs(save_dir)
             print("Created projections directory", flush=True)
         f_aligned = f[:-4] + "_stack.npy"
+        stack_1 = None
+        name_1 = None
         # if this run is for reference creation, we first check whether a realigned stack already exists
         if is_ref and path.exists(f_aligned):
             stack = np.load(f_aligned).astype(np.float32)
         else:
             stack = OpenStack(f).astype(np.float32)
-            stack = ReAlign(stack, 4 * zoom_level)[0]
+            if co_align:
+                name_1 = f[:-6]+"_1.tif"
+                stack_1 = OpenStack(name_1)
+                out = ReAlign(stack, 4 * zoom_level, stack_1)
+                stack = out[0]
+                stack_1 = out[-1]
+            else:
+                stack = ReAlign(stack, 4 * zoom_level)[0]
         if not is_ref:
             np.save(f_aligned, stack.astype(np.uint8))
         print("Stack realigned", flush=True)
@@ -63,3 +86,8 @@ if __name__ == "__main__":
             projection = np.sum(stack, 0)
             projection = cutOutliers(projection, 99.9) * (2**16-1)
             cv2.imwrite(save_dir + "/" + "MAX_" + stackName, projection.astype(np.uint16))
+            if co_align:
+                projection = np.sum(stack_1, 0)
+                projection = cutOutliers(projection, 99.9) * (2**16-1)
+                stack_1_Name = path.split(name_1)[1]
+                cv2.imwrite(save_dir + "/" + "MAX_" + stack_1_Name, projection.astype(np.uint16))
